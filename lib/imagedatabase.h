c114348b6cf1f96834d57e480527101cdc523312
dungpt3003 <dungpt3003@gmail.com>
2016-07-19 15:45:40 +0800

    Add class to load and save data

--- lib/imagedatabase.h
+++ lib/imagedatabase.h
@@ -2,6 +2,8 @@
 #define IMAGEDATABASE_H
 
 #include "common.h"
+#include <iostream>
+using namespace std;
 
 // Stores the datasets used for training and testing the Support
 // Vector Machine class. Contains basically a list of image filenames
@@ -18,13 +20,70 @@ private:
 
 public:
 	// Create a new database.
-	ImageDatabase();
-	ImageDatabase(const char* dbFilename);
-	ImageDatabase(const std::vector<float>& labels, const std::vector<std::string>& filenames);
+	ImageDatabase():
+	_negativesCount(0), _positivesCount(0)
+	{
+	}
 
+	//ImageDatabase(const char* dbFilename);
+	ImageDatabase(const char* dbFilename):
+	_negativesCount(0), _positivesCount(0)
+	{
+		load(dbFilename);
+	}
+
+	ImageDatabase(const vector<float>& labels, const vector<string>& filenames):
+	_negativesCount(0), _positivesCount(0)
+	{
+		_labels = labels;
+		_filenames = filenames;
+
+		for(vector<float>::iterator i = _labels.begin(); i != _labels.end(); i++) {
+			if(*i > 0) _positivesCount++;
+			else if(*i < 0) _negativesCount++;
+		}
+	}
 	// Load a database from file.
-	void load(const char* dbFilename);
-	void save(const char* dbFilename);
+	void load(const char *dbFilename)
+	{
+		_dbFilename = string(dbFilename);
+
+		_negativesCount = 0;
+		_positivesCount = 0;
+
+		ifstream f(dbFilename);
+		if(!f.is_open()) {
+			printf("Could not open file %s for reading", dbFilename);
+		}
+
+		int nItems;
+		f >> nItems;
+
+		assert(nItems > 0);
+		_labels.resize(nItems);
+		_filenames.resize(nItems);
+
+		for(int i = 0; i < nItems; i++) {
+			f >> _labels[i] >> _filenames[i];
+
+			if(_labels[i] < 0) _negativesCount++;
+			else if(_labels[i] > 0) _positivesCount++;
+		}
+	}
+
+	// Save database to a file
+	void save(const char* dbFilename)
+	{
+		ofstream f(dbFilename);
+		if(!f.is_open()) {
+			printf("Could not open file %s for writing", dbFilename);
+		}
+
+		f << _labels.size() << "\n";
+		for(int i = 0; i < _labels.size(); i++) {
+			f << _labels[i] << " " << _filenames[i] << "\n";
+		}
+	}
 
 	// Accessors
 	const int getLabel(int idx) const { return _labels[idx]; }
@@ -42,6 +101,16 @@ public:
 
 // Prints information about the dataset
 std::ostream&
-operator<<(std::ostream& s, const ImageDatabase& db);
+operator<<(std::ostream& s, const ImageDatabase& db)
+{
+	s << "DATABASE INFO\n"
+	  << setw(20) << "Original filename:" << " " << db.getDatabaseFilename() << "\n"
+	  << setw(20) << "Positives:" << setw(5) << right << db.getPositivesCount() << "\n"
+	  << setw(20) << "Negaties:"   << setw(5) << right << db.getNegativesCount() << "\n"
+	  << setw(20) << "Unlabeled:"  << setw(5) << right << db.getUnlabeledCount() << "\n"
+	  << setw(20) << "Total:"      << setw(5) << right << db.getSize() << "\n";
+
+	return s;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "lib/common.h"
+#include "lib/ImageDatabase.h"
 
 using namespace cv;
+using namespace std;
+
+
+int readDatabse(int argc, char** argv){
+  const char* dbFName = argv[1];
+  ImageDatabase db(dbFName);
+  cout << db << endl;
+  return EXIT_SUCCESS;
+}
 
 int main(int argc, char** argv )
 {
     if ( argc != 2 )
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
+        printf("Usage: ./pd path_to_database");
         return -1;
     }
 
-    Mat image;
-    image = imread( argv[1], 1 );
-
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-
-    waitKey(0);
-
-    return 0;
+    return readDatabse(argc, argv);
 }
