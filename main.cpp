368b834cc1159c2d4e5a31ba117ce9b5d3c58f74
dungpt3003 <dungpt3003@gmail.com>
2016-07-20 15:06:58 +0800

    Add function to get files in directory

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <dirent.h>
+#ifdef __MINGW32__
+#include <sys/stat.h>
+#endif
+#include <ios>
+#include <fstream>
+#include <stdexcept>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
@@ -68,6 +75,44 @@ static void saveDescriptorVectorToFile(vector<float>& descriptorVector, vector<u
         File.flush();
         File.close();
     }
+}
+
+static void getFilesInDirectory(const string& dirName, vector<string>& fileNames, const vector<string>& validExtensions) {
+    printf("Opening directory %s\n", dirName.c_str());
+#ifdef __MINGW32__
+	struct stat s;
+#endif
+    struct dirent* ep;
+    size_t extensionLocation;
+    DIR* dp = opendir(dirName.c_str());
+    if (dp != NULL) {
+        while ((ep = readdir(dp))) {
+            // Ignore (sub-)directories like . , .. , .svn, etc.
+#ifdef __MINGW32__
+			stat(ep->d_name, &s);
+			if (s.st_mode & S_IFDIR) {
+				continue;
+			}
+#else
+            if (ep->d_type & DT_DIR) {
+                continue;
+            }
+#endif
+            extensionLocation = string(ep->d_name).find_last_of("."); // Assume the last point marks beginning of extension like file.ext
+            // Check if extension is matching the wanted ones
+            string tempExt = toLowerCase(string(ep->d_name).substr(extensionLocation + 1));
+            if (find(validExtensions.begin(), validExtensions.end(), tempExt) != validExtensions.end()) {
+                printf("Found matching data file '%s'\n", ep->d_name);
+                fileNames.push_back((string) dirName + ep->d_name);
+            } else {
+                printf("Found file does not match required file type, skipping: '%s'\n", ep->d_name);
+            }
+        }
+        (void) closedir(dp);
+    } else {
+        printf("Error opening directory '%s'!\n", dirName.c_str());
+    }
+    return;
 }
 
 int main(int argc, char** argv )
